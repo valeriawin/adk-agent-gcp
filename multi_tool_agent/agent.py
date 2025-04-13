101405c894c1ef482ffe6cdfacca946f2c961ac0
valeria-imobility <baranova.valeriya.by@gmail.com>
2025-04-13 17:27:35 +0200

    update to multiple agents

--- multi_tool_agent/agent.py
+++ multi_tool_agent/agent.py
@@ -1,47 +1,44 @@
-from dotenv import load_dotenv
 from google.adk.agents import Agent
-from langchain_google_community import GoogleSearchAPIWrapper
-from langchain_community.tools.ddg_search import DuckDuckGoSearchRun
+from google.adk.tools import google_search, agent_tool
 from langchain_community.tools.shell.tool import ShellTool
 
 
-load_dotenv()
+search_agent = Agent(
+    model='gemini-2.0-flash-exp',
+    name='search_agent',
+    instruction='You are a specialist in Google Search.',
+    tools=[google_search]
+)
 
-def limited_search(query: str) -> dict:
-    """Search the web for information.
-
-    Args:
-        query (str): The query for the search engine to search.
-
-    Returns:
-        dict: status and result or error msg.
-    """
-    # search = GoogleSearchAPIWrapper(google_cse_id=os.getenv("GOOGLE_CSE_ID"), google_api_key=os.getenv("GOOGLE_API_KEY"))
-    search = DuckDuckGoSearchRun()
-    return {"status": "success", "report": search.run(query)}
-
-
-def limited_shell(command: str) -> str:
+def limited_shell(command: str) -> dict:
     """Execute shell commands on the local machine.
     Use this tool carefully and only when asked to perform local system operations."""
     shell_tool = ShellTool()
 
     # safety check for potentially harmful commands
-    dangerous_commands = ["rm", "mkfs", "dd", ":(){", "sudo", ">", "shutdown"]
+    dangerous_commands = ['rm', 'mkfs', 'dd', ':(){', 'sudo', '>', 'shutdown']
     if any(cmd in command for cmd in dangerous_commands):
-        return "For safety reasons, I cannot execute potentially harmful commands like deletion, formatting, or system commands."
+        error_msg = 'For safety reasons, I cannot execute potentially harmful commands.'
+        return {'status': 'error', 'data': error_msg}
 
-    return shell_tool.run(command)
+    return {'status': 'success', 'data': shell_tool.run(command)}
+
+shell_agent = Agent(
+    model='gemini-2.5-pro-exp-03-25',
+    name='shell_agent',
+    instruction='You are a specialist in executing Shell commands.',
+    tools=[limited_shell]
+)
 
 root_agent = Agent(
-    name="rag_agent",
-    model="gemini-2.5-pro-exp-03-25",
-    # model="gemini-2.0-flash",
+    name='my_agent',
+    model='gemini-2.5-pro-exp-03-25',
+    # model='gemini-2.0-flash',
     description=(
-        "Agent to answer questions using search engine and run commands in shell."
+        'Agent to answer questions using search engine and run commands in shell.'
     ),
     instruction=(
-        "I can search any information and prepare an answer. I can also run commands in shell terminal."
+        'I can search any information and prepare an answer. I can also run commands in shell terminal.'
     ),
-    tools=[limited_search, limited_shell],
+    tools=[agent_tool.AgentTool(agent=search_agent), agent_tool.AgentTool(agent=shell_agent)],
 )
